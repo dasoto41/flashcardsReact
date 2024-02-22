baf8649076433d5c9a461526b6b7376ebf2b826c
daniela soto <danysoto41@gmail.com>
2024-02-22 16:29:16 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+const questions = [
+  {
+    id: 1,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 2,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 3,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 4,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 5,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 6,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  const [idquestion, setIdquestion] = useState(null);
+
+  function handleClick(id) {
+    setIdquestion(id !== idquestion ? id : "null");
+    console.log(id);
+  }
+  return (
+    <div className="flashcards">
+      {questions.map((question) => (
+        <div
+          className={question.id === idquestion ? "selected" : ""}
+          onClick={() => handleClick(question.id)}
+        >
+          <p>
+            {question.id === idquestion ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
